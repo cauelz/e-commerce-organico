d1a6d9659ec1f2bc6a0849eddc38b72ac22d6c54
Jesus Alejandro <jesusariasgodoy2508@gmail.com>
2020-11-14 21:27:40 -0300

    cards Jesus

---
+++ src/components/Header/cards/cardGrid.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from '../Components/card'
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+
+const CardGrid = () => {
+    return(
+        <div className="container-fluid d-flex justify-content-center">
+            <div className="row">
+                <div className="col-md-4">
+                    <Card imgsrc={img1} />
+                </div>
+                <div className="col-md-4">
+                    <Card imgsrc={img2} />
+                </div>
+                <div className="col-md-4">
+                    <Card imgsrc={img3} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CardGrid;
