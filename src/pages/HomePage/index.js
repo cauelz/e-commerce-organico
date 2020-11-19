e08c550b75cfc5d36b78b7ae0bbebc5f4cea9ad6
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2020-11-19 09:59:37 -0300

    só pra commitar

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import Header from '../../components/Header/Header';
-
+import React from "react";
+import Header from "../../components/Header/Header";
+import CarouselHome from '../../components/Carousel/Carousel'
 
 const HomePage = () => {
-    return (
-        <>
-        <Header></Header>
-        </>
-    );
+  return (
+    <>
+      <Header></Header>
+      <CarouselHome />
+    </>
+  );
 }
 
 export default HomePage;
