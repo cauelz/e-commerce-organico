a6524a8b14b20776bc2858732df4e81a4eddc9ec
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2020-11-23 11:11:30 -0300

    feature: adicionei modal com login e cadastro

--- src/components/Header/LoginModal/LoginModal.js
+++ src/components/Header/LoginModal/LoginModal.js
@@ -1,35 +1,81 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
-
-
+import React from "react";
+import { Modal, Button, FormControl, Container } from "react-bootstrap";
+import "../style.css";
 
 const LoginModal = (props) => {
-
-    return (
-      <Modal
-        {...props}
-        size="xl"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
-          </Modal.Title>
-        </Modal.Header>
+  return (
+    <Modal
+      {...props}
+      size="xl"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Faça login ou cadastre-se
+        </Modal.Title>
+      </Modal.Header>
+      <Container className="d-flex">
         <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
+          <h4 className="m-3">Dados de Login</h4>
+          <FormControl
+            className="m-3"
+            placeholder="Usuário"
+            aria-label="Usuário"
+            aria-describedby="basic-addon2"
+          />
+          <FormControl
+            className="m-3"
+            type="password"
+            placeholder="Senha"
+            aria-label="Senha"
+            aria-describedby="basic-addon2"
+          />
+          <Button type="submit" variant="outline-success" className="mx-3">Entrar</Button>
+          <Button type="submit" variant="link" className="mx-1" size="sm">esqueci minha senha</Button>
         </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+        <Modal.Body>
+          <h4 className="m-3">Dados de Cadastro</h4>
+          <FormControl
+            className="m-3"
+            placeholder="Primeiro Nome"
+            aria-label="Primeiro Nome"
+            aria-describedby="basic-addon2"
+          />
+          <FormControl
+            className="m-3"
+            placeholder="Último Nome"
+            aria-label="Último Nome"
+            aria-describedby="basic-addon2"
+          />
+          <FormControl
+            className="m-3"
+            placeholder="E-mail"
+            aria-label="E-mail"
+            aria-describedby="basic-addon2"
+          />
+          <FormControl
+            className="m-3"
+            type="password"
+            placeholder="Senha"
+            aria-label="Senha"
+            aria-describedby="basic-addon2"
+          />
+          <FormControl
+            className="m-3"
+            type="password"
+            placeholder="Confirmação de Senha"
+            aria-label="Confirmação de Senha"
+            aria-describedby="basic-addon2"
+          />
+          <Button type="submit" variant="outline-success" className="mx-3">Cadastre-se</Button>
+        </Modal.Body>
+      </Container>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
-  export default LoginModal;
+export default LoginModal;
