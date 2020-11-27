52ca32f59a0fd992aeeb7d95d18ea6bc46a715ea
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2020-11-27 10:27:19 -0300

    feature: adicionado userpage e componente Form

---
+++ src/pages/UserPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer'
+
+
+const HomePage = () => {
+    return (
+      <>
+        <Header></Header>
+        <body></body>
+        <Footer></Footer>
+      </>
+    );
+}
+
+export default HomePage;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-
+import UserPage from './pages/UserPage';
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={HomePage} />
+        <Route path="/user" component={UserPage}/>
       </Switch>
     </BrowserRouter>
   );
