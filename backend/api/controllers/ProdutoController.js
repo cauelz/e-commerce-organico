e3e1862cf648dab9c981a48a8401fbd40a70e69a
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-01-13 16:50:36 -0300

    feature: metodos atualizar e deletar adicionados ao controller e rota de delete criada

--- backend/api/controllers/ProdutoController.js
+++ backend/api/controllers/ProdutoController.js
@@ -36,6 +36,68 @@ class ProdutoController {
 
         }
     }
+
+    static async criaProduto(req, resp) {
+        const novoProduto = req.body;
+        try {
+
+            const novoProdutoCriado = await database.Produtos.create(novoProduto);
+            return resp.status(200).json(novoProdutoCriado);
+
+        } catch (error) {
+
+            return resp.status(500).json(error.message);
+
+        }
+    }
+
+    static async atualizaProduto(req, resp) {
+        const { id } = req.params;
+        const novasInfos = req.body;
+
+        try {
+
+            await database.Produtos.update(novasInfos, {
+                where: {
+                    id: id
+                }
+            });
+
+            const produtoAtualizado = await database.Produtos.findOne(
+                {
+                    where: {
+                        id: Number(id)
+                    }
+                }
+            )
+
+            return resp.status(200).json(produtoAtualizado);
+
+        } catch (error) {
+
+            return resp.status(500).json(error.message);
+
+        }
+    }
+
+    static async deletaProduto(req, resp) {
+        const { id } = req.params;
+        try {
+
+            await database.Produtos.destroy({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return resp.status(200).json({
+                mensagem: `id ${id} deletado`
+            })
+
+        } catch (error) {
+            return resp.status(200).json(error.message);
+        }
+    }
 }
 
 module.exports = ProdutoController;
--- backend/api/routes/produtosRoute.js
+++ backend/api/routes/produtosRoute.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.get('/produtos', ProdutoController.pegarProdutos);
 router.get('/produtos/:id', ProdutoController.pegaUmProduto);
+router.post('/produtos', ProdutoController.criaProduto);
+router.put('/produtos/:id', ProdutoController.atualizaProduto);
+router.delete('/produtos/:id', ProdutoController.deletaProduto);
 
 module.exports = router;
