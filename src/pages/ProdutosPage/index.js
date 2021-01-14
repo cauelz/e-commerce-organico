8794dbba3b028b8e511a86c410424e7d3086b98c
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-01-14 14:48:03 -0300

    feature: adicionado componente rating

--- src/pages/ProdutosPage/index.js
+++ src/pages/ProdutosPage/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import Product from '../../components/Product/Product';
 import products from '../../products';
 
 const ProdutosPage = () => {
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => {
-          return (
-            <Col sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          )
-        })}
-      </Row>
+      <Container>
+        <hr></hr>
+        <h1>Latest Products</h1>
+        <Row>
+          {products.map((product) => {
+            return (
+              <Col sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
     </>
   )
 }
