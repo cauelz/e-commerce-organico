656fea1045966bd67d80a5656a092a21f7b31be8
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-01-15 17:33:23 -0300

    feature: adicionado conexão com mongoDB, instalado pacotes necessários e criado modelos

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const colors = require('colors');
 const products = require('./data/products');
-const app = express();
 
+dotenv.config();
+
+connectDB();
+
+const app = express();
 
 app.get('/', (req, resp) => {
   resp.send('Api is running!');
@@ -16,4 +23,6 @@ app.get('/api/products/:id', (req, resp) => {
   resp.json(product);
 })
 
-app.listen(5000, console.log('Server is running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`));
