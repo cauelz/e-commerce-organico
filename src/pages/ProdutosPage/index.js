cb0b040a6475541823379d18b3685cd83a8b0ca9
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-01-16 19:22:06 -0300

    feature: reducer da página de listagem de produtos criada com sucesso

--- src/pages/ProdutosPage/index.js
+++ src/pages/ProdutosPage/index.js
@@ -1,36 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Col, Container, Row } from 'react-bootstrap';
 import Product from '../../components/Product/Product';
-import axios from 'axios';
+import { listProducts } from '../../actions/productActions';
 
 const ProdutosPage = () => {
+  const dispatch = useDispatch();
 
-  const [products, setProducts] = useState([]);
+  const productList = useSelector(state => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    }
-
-    fetchProducts()
-
-  }, [])
+    dispatch(listProducts());
+  }, [dispatch])
 
   return (
     <>
       <Container>
         <hr></hr>
-        <h1>Latest Products</h1>
-        <Row>
-          {products.map((product) => {
-            return (
-              <Col sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            )
-          })}
-        </Row>
+        <h1>Últimos Produtos</h1>
+
+        {loading ? (<h2>Carregando página...</h2>)
+          : error ?
+            (<h3>{error}</h3>)
+            : (
+              <Row>
+                {products.map((product) => {
+                  return (
+                    <Col sm={12} md={6} lg={4} xl={3}>
+                      <Product product={product} />
+                    </Col>
+                  )
+                })}
+              </Row>
+            )}
       </Container>
     </>
   )
