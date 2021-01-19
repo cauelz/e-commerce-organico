d666b30f9929d1a90c72c528ad5c85973c619f6d
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-01-19 14:58:07 -0300

    feature: criadas constantes, reducers e ações para o login de usuários

--- src/pages/CarrinhoPage/CarrinhoPage.js
+++ src/pages/CarrinhoPage/CarrinhoPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+import { Row, Col, ListGroup, Form, Button, Card } from 'react-bootstrap';
 import Message from '../../components/Message/Message';
 import { addToCart, removeFromCart } from '../../actions/cartActions';
 
