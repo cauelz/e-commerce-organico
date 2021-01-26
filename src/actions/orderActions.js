756c929350763011ff67aa975545211c7ed1f4e6
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-01-26 12:04:37 -0300

    feature: detalhes dos pedidos por usuario

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -7,7 +7,10 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
-  ORDER_PAY_FAIL
+  ORDER_PAY_FAIL,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_FAIL
 } from '../constants/orderConstants';
 import axios from 'axios';
 
@@ -98,6 +101,37 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
   } catch (error) {
     dispatch({
       type: ORDER_PAY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const listMyOrders = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_LIST_MY_REQUEST,
+    })
+
+    const { userLogin: { userInfo } } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+    const { data } = await axios.get(`/api/orders/myorders`, config);
+
+    dispatch({
+      type: ORDER_LIST_MY_SUCCESS,
+      payload: data,
+    })
+
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_MY_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,6 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import CarouselDefault from './components/Carousel/Carousel';
 import HomePage from "./pages/HomePage";
-import UserPage from './pages/UserPage';
-import CadastroPage from './pages/CadastroPage';
-import AtendimentoPage from './pages/AtendimentoPage';
-import NotificacoesPage from './pages/NotificacoesPage';
-import AddCreditCard from './components/Forms/AddCreditCard';
 import BlogPage from "./pages/Blog";
 import DetProduto from "./pages/DetProdutoPage";
 import ProdutosPage from './pages/ProdutosPage';
@@ -42,11 +37,6 @@ const Routes = () => {
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/register" component={RegisterPage} />
           <Route exact path="/profile" component={ProfilePage} />
-          <Route exact path="/user/cadastro" component={CadastroPage} />
-          <Route exact path="/user/cartao" component={AddCreditCard} />
-          <Route exact path="/user/atendimento" component={AtendimentoPage} />
-          <Route exact path="/user/notificacoes" component={NotificacoesPage} />
-          <Route exact path="/user" component={UserPage} />
           <Route path="/" component={HomePage} />
         </Switch>
       </main>
