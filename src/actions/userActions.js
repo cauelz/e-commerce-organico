fac5730a01be754d83ed65b132f003541442f29e
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-01-26 15:07:20 -0300

    bugfix: cada perfil tem os seus dados de perfil

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,6 @@
 import axios from "axios"
-import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS } from "../constants/userConstant"
+import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_RESET, USER_DETAILS_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS } from "../constants/userConstant"
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -36,6 +37,12 @@ export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   dispatch({
     type: USER_LOGOUT
+  })
+  dispatch({
+    type: USER_DETAILS_RESET
+  })
+  dispatch({
+    type: ORDER_LIST_MY_RESET
   })
 }
 
